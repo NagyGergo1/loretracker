ed00797555f63e408d7bd6d233952fbae1e40f51
Fodor Endre <74898172+Eleckt3452@users.noreply.github.com>
2025-03-18 00:45:46 +0100

    Login page is mostmár a felhasználó adatokat mutatja ha be van jelentkezve

--- js/loginPage.js
+++ js/loginPage.js
@@ -2,9 +2,50 @@ import { callphpFunction } from "./index.js";
 import { setCookie } from "./index.js";
 import { getCookie } from "./index.js";
 import { checkCookie } from "./index.js";
+import { loginStat } from "./index.js";
+import { deleteCookie } from "./index.js";
 
 function $(id) {
     return document.getElementById(id);
+}
+
+async function loadPage() {
+    checkCookie("name");
+    console.log(loginStat);
+    if (loginStat == true) {
+        $("form-content").innerHTML = "";
+        let userName = getCookie("name");
+        let userData = await callphpFunction("getUserByName", {name: userName});
+        console.log(userData);
+        let table = document.createElement("table");
+        table.classList.add("table");
+        
+        table.innerHTML += `
+            <tr>
+                <th>Felhasználónév: </th>
+                <td>${userData.userName}</td>
+            </tr>
+            <tr>
+                <th>E-mail: </th>
+                <td>${userData.email}</td>
+            </tr>
+            <tr>
+                <th>SteamID: </th>
+                <td>${userData.steamID}</td>
+            </tr>
+        `;
+        let button = document.createElement("button");
+        button.classList.add("btn");
+        button.classList.add("btn-danger");
+        button.id = ("kilepes");
+        button.type = "button";
+        button.innerHTML = "Kilépés";
+
+        $("form-content").appendChild(table);
+        $("form-content").appendChild(button);
+
+        $("kilepes").addEventListener("click", deleteCookie("name"));
+    }
 }
 
 async function bejelentkezes() {
@@ -42,5 +83,9 @@ async function bejelentkezes() {
     }
 
 }
+
+window.addEventListener("load", function() {
+    loadPage();
+})
 
 $("inditas").addEventListener("click", bejelentkezes);
