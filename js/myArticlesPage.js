d3aaf369e7bdd161a7de80a5877a108fc2866b6b
Fodor Endre <74898172+Eleckt3452@users.noreply.github.com>
2025-04-11 18:30:37 +0200

    Saját cikkek oldal elkezdése

--- js/myArticlesPage.js
+++ js/myArticlesPage.js
@@ -2,4 +2,16 @@ import { $ } from "./index.js";
 import { callphpFunction } from "./index.js";
 import { steamRequest, loginStat, checkCookie, getCookie, loginCheck } from "./index.js";
 
+async function loadPosts() {
+    try {
+        let userData = getCookie("userId");
+
+        let userArticles = callphpFunction("getAdditionalByUser", {userID : userData});
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 window.addEventListener('load', loginCheck)
